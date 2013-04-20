67d4234ec09b4b689c093cdbda6817c4b174c2dc
VikramTiwari <Vikramtheone1@gmail.com>
2013-04-21 02:07:09 +0530

    Interrupts
    Segmentation and Interrupts

--- src/main.c
+++ src/main.c
@@ -2,13 +2,19 @@
 // Defines the C-code kernel entry points, calls intialization routines.
 
 #include "monitor.h"
+#include "descriptor_tables.h"
 
 int main(struct multiboot *mboot_ptr)
 {
+	// Initialize all the ISRs and Segmentation
+	init_descriptor_tables();
 	// Initialize the screen by clearing it
 	monitor_clear();
 	// Write out a basic string
-	monitor_write("Hello World!");
+	monitor_write("Hello World! \n");
+
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
 
 	return 0;
 }
